d3b41ad0602cb087584662418dff6003104c08aa
KueTone <66387900+KueTone@users.noreply.github.com>
2023-04-28 10:25:21 -0700

    Login and logout basic functionality

--- run.py
+++ run.py
@@ -1,1 +1,40 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# @app.route('/')
+# def welcome():
+#     return render_template('welcome.html')
+# # Route for handling the login page logic
+
+
+# Route for handling the login page logic
+@app.route('/welcome', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        # future change will incorporate a simple textfile database and maybe a SQL database if provded time
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/editProfile')
+def editProfile():
+    return render_template('editProfile.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

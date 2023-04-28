e27a81b3311a76c8db8a998772484542fa3822cf
Dorianv5 <118552196+Dorianv5@users.noreply.github.com>
2023-04-28 13:25:28 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,51 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
 
-from flask import Flask
-from flask import escape
 
-myapp_obj = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@myapp_obj.route("/")
-@myapp_obj.route("/home.html")
-def index():
-    return "home"
 
-@myapp_obj.route("/hello")
-def hello():
-    return "Hello World!"
+# @app.route('/')
+# def welcome():
+#     return render_template('welcome.html')
+# # Route for handling the login page logic
 
-@myapp_obj.route("/login")
+
+# Route for handling the login page logic
+@app.route('/welcome', methods=['GET', 'POST'])
 def login():
-    return "Login Page!"
+    error = None
+    if request.method == 'POST':
+        # future change will incorporate a simple textfile database and maybe a SQL database if provded time
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
-@myapp_obj.route("/members/<string:name>/")
-def getMember(name):
-    return escape(name)
 
-myapp_obj.run()
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/editProfile')
+def editProfile():
+    return render_template('editProfile.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def createAcct():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] == 'admin':
+            error = 'Username already in use. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('acctCreate.html', error = error)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
